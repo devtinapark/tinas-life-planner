5b6e80a15255ad702654b3df1f91670b702e8e8d
Christine Tina Park <escp1004jk@gmail.com>
2021-10-17 19:04:09 -0700

    Completed issue-5-view-quote-of-the-day. - Christine Tina Park

--- js/data.js
+++ js/data.js
@@ -1,1 +1,16 @@
 /* exported data */
+var $textQuote = document.querySelector('#textQuote');
+var $authorQuote = document.querySelector('#authorQuote');
+
+function renderQuote() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://favqs.com/api/qotd');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    $textQuote.textContent = xhr.response.quote.body;
+    $authorQuote.textContent = '- ' + xhr.response.quote.author;
+  });
+  xhr.send();
+}
+
+renderQuote();
